0f73b7602755339646c7f6ee0c5e1c3a69f2b38d
Tuszmak <daniel.pinter02@gmail.com>
2024-01-27 19:55:04 +0100

    fix: fix delete query

--- db/queries.py
+++ db/queries.py
@@ -1,4 +1,4 @@
 insert_query = "INSERT INTO secrets (hash, secret_text, created_at, expires_at, remaining_views) VALUES (%s, %s, %s, %s, %s);"
 getQuery = "SELECT * FROM secrets WHERE hash = %s;"
 updateQuery = "UPDATE secrets SET remaining_views = %s WHERE hash = %s"
-deleteQuery = "DELETE secret FROM secrets WHERE hash = %s;"
+deleteQuery = "DELETE FROM secrets WHERE hash = %s;"
