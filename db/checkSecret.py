dd53d1e8a3cddce1436af52e3f278e5a659d557f
Tuszmak <daniel.pinter02@gmail.com>
2024-01-27 19:51:09 +0100

    fix: fix secret availability check

--- db/checkSecret.py
+++ db/checkSecret.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from model import SecretDao
 def check_secret(secret: SecretDao):
+    print(check_time(secret.expires_at) and check_visit_number(secret.remainingViews))
     return check_time(secret.expires_at) and check_visit_number(secret.remainingViews)
 
 def check_time(time: datetime):
-    return time < datetime.now()
+    return time > datetime.now()
 def check_visit_number(number):
     return number > 0
