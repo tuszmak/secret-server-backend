ead0d3540bc087585738bf055c19a081186e0402
Tuszmak <daniel.pinter02@gmail.com>
2024-01-27 19:55:21 +0100

    refactor: give delete query only the hash

--- service/updateSecretService.py
+++ service/updateSecretService.py
@@ -1,13 +1,15 @@
 from db.checkSecret import check_secret
 from db.updateSecret import update_secret_in_db
 from .deleteSecretService import delete_secret
+from model import SecretDao
 
-def update_secret(secret, envVariables):
+def update_secret(secret: SecretDao, envVariables):
     try:
         if(check_secret(secret)):
             update_secret_in_db(secret, envVariables)
         else:
-            delete_secret(secret, envVariables)
+            delete_secret(secret.hash, envVariables)
     except:
+        raise
         raise Exception("Can't update the secret")
 
