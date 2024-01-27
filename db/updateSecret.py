a9191ce14950fe6c4b232701357d4d410e74ff4b
Tuszmak <daniel.pinter02@gmail.com>
2024-01-27 19:47:43 +0100

    feat: create updating secret

--- db/updateSecret.py
+++ db/updateSecret.py
@@ -1,2 +1,14 @@
-def updateSecret(secret):
-    print()
+from .getConn import getConn
+from .queries import updateQuery
+from model import SecretDao
+
+def update_secret_in_db(secret: SecretDao, envVariables): 
+    conn = getConn(envVariables)
+    cur = conn.cursor()
+    try:
+              cur.execute(updateQuery, (secret.remainingViews -1,secret.hash)) # paramList: new views, hash              
+              conn.commit()
+    except Exception: 
+              raise Exception("Update query can't be executed")
+    cur.close()
+    
--- service/updateSecretService.py
+++ service/updateSecretService.py
@@ -1,2 +1,13 @@
-def deleteSecret(secret):
-    print()
+from db.checkSecret import check_secret
+from db.updateSecret import update_secret_in_db
+from .deleteSecretService import delete_secret
+
+def update_secret(secret, envVariables):
+    try:
+        if(check_secret(secret)):
+            update_secret_in_db(secret, envVariables)
+        else:
+            delete_secret(secret, envVariables)
+    except:
+        raise Exception("Can't update the secret")
+
