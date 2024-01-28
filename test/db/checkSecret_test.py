9c619b120c7485addc3490871deb5abb92a86b65
Tuszmak <daniel.pinter02@gmail.com>
2024-01-28 12:00:04 +0100

    feat: add tests for secret checks

---
+++ test/db/checkSecret_test.py
@@ -0,0 +1,25 @@
+import pytest
+from model import SecretDao
+from datetime import datetime
+from db.checkSecret import check_secret
+
+class Test_CheckSecret():
+    def test_check_when_date_is_old(self):
+        foo = SecretDao(2,"foo","foo",datetime.now(), datetime(1984,5,5,2,2,0),5)
+        expected = False
+        actual = check_secret(foo)
+        assert expected == actual
+    def test_check_when_visit_is_zero(self):
+        foo = SecretDao(2,"foo","foo",datetime.now(), datetime(2055,5,5,2,2,0),0)
+        expected = False
+        actual = check_secret(foo)
+        assert expected == actual
+    def test_check_when_all_good(self):
+        foo = SecretDao(2,"foo","foo",datetime.now(), datetime(2055,5,5,2,2,0),5)
+        expected = True
+        actual = check_secret(foo)
+        assert expected == actual
+    def test_check_when_no_data_given(self):
+        with pytest.raises(Exception) as exc_info:
+            check_secret(None)
+            assert exc_info == "No data given"
