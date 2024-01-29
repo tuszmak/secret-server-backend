f163555ec58e163bf9d38f08c6a651524e5471b4
Tuszmak <daniel.pinter02@gmail.com>
2024-01-29 12:19:47 +0100

    fix: fix visits not updating

--- test/service/getSecretService_test.py
+++ test/service/getSecretService_test.py
@@ -10,11 +10,14 @@ class TestGetSecretService():
                ) as mock_fetch,patch(
                    "service.get_secret_service.to_secret_dao"
                    ) as mock_dao, patch(
+                       "service.get_secret_service.update_secret"
+                       ) as mock_update, patch(
                            "service.get_secret_service.decrypt_secret", return_value={"secret": "bar"}
                            ) as mock_decrypt:
-            get_secret_by_hash({"secret": "foo"}, None)
+            get_secret_by_hash("foo", None)
             mock_fetch.assert_called_once()
             mock_dao.assert_called_once()
+            mock_update.assert_called()
             mock_decrypt.assert_called_once()
 
     def test_fail(self):
@@ -23,12 +26,15 @@ class TestGetSecretService():
                ) as mock_fetch,patch(
                    "service.get_secret_service.to_secret_dao"
                    ) as mock_dao, patch(
+                       "service.get_secret_service.update_secret"
+                       ) as mock_update, patch(
                            "service.get_secret_service.decrypt_secret", side_effect=Exception("foo")
                            ) as mock_decrypt:
             with pytest.raises(Exception) as exc_info:
-                get_secret_by_hash({"secret": "foo"}, None)
+                get_secret_by_hash("foo", None)
                 assert exc_info == "foo"
             mock_fetch.assert_called_once()
             mock_dao.assert_called_once()
+            mock_update.assert_called()
             mock_decrypt.assert_called_once()
                               
